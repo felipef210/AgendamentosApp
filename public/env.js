832e1efc93eb55f2f22a6146cd17ff7cc622ee42
Felipe Miranda <felipe.santos@mosten.com>
2025-10-31 10:55:35 -0300

    style: Adição de animações no carregamento das páginas

--- public/env.js
+++
@@ -1,4 +0,0 @@
-(function(window) {
-  window["env"] = window["env"] || {};
-  window["env"]["NG_APP_API_URL"] = `${NG_APP_API_URL}`;
-})(this);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'AgendamentosApp';
+  protected readonly title = signal('AgendamentosApp');
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init();
+    }
+  }
 }
